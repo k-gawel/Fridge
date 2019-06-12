c53718db2df09d737577c99d14643d59dc68ca21
Kamil <kamil.gawel.aplikacje@gmail.com>
2019-06-12 12:22:03 +0200

    hibernate configuration moved do hibernate.properties

--- src/main/java/org/california/config/HibernateUtil.java
+++ src/main/java/org/california/config/HibernateUtil.java
@@ -1,12 +1,13 @@
 package org.california.config;
 
-import org.apache.commons.dbcp.BasicDataSource;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.cfg.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -18,45 +19,40 @@ import java.util.Properties;
 @Configuration
 @EnableTransactionManagement
 @ComponentScan("org.california")
+@PropertySource("classpath:hibernate.properties")
 public class HibernateUtil {
 
     public StandardServiceRegistry registry;
     public SessionFactory sessionFactory;
 
-
     @Bean
-    public DataSource getDataSource()  {
-        BasicDataSource dataSource = new BasicDataSource();
+    public static DataSource getDataSource() {
+        Properties props = Environment.getProperties();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (Exception e) {
-        }
-
-        dataSource.setUrl("jdbc:mysql://localhost:3306/fridge?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
-        dataSource.setPoolPreparedStatements(true);
+        dataSource.setDriverClassName(props.getProperty(Environment.DRIVER));
+        dataSource.setUrl(props.getProperty(Environment.URL));
+        dataSource.setUsername(props.getProperty(Environment.USER));
+        dataSource.setPassword(props.getProperty(Environment.PASS));
 
         return dataSource;
     }
 
+
     @Bean
-    public LocalSessionFactoryBean getSessionFactory()  {
+    public LocalSessionFactoryBean getSessionFactory() {
+
         LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
-        factoryBean.setDataSource(getDataSource());
-
-        Properties settings = new Properties();
-
-        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-        settings.put(Environment.HBM2DDL_AUTO, "update");
-        settings.put(Environment.SHOW_SQL, "false");
+        Properties settings = Environment.getProperties();
 
         factoryBean.setHibernateProperties(settings);
+        factoryBean.setDataSource(getDataSource());
         factoryBean.setPackagesToScan("org.california.model", "java.util");
 
         return factoryBean;
     }
+
+
 
     @Bean
     public HibernateTransactionManager hibernateTransactionManager() {
