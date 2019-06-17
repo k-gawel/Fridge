a08294de423eb5da07f3f401d8c936bfec574429
Kamil <kamil.gawel.aplikacje@gmail.com>
2019-06-17 13:31:02 +0200

    dto mapper improved

--- src/main/java/org/california/model/transfer/response/EntityToDtoMapper.java
+++ src/main/java/org/california/model/transfer/response/EntityToDtoMapper.java
@@ -2,21 +2,24 @@ package org.california.model.transfer.response;
 
 import org.california.model.entity.*;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
 public class EntityToDtoMapper {
 
-    public PlaceDto placeToDto(Place place) {
-        Collection<ContainerDto> containers = place.getContainers().stream().map(this::containerToDto).collect(Collectors.toList());;
+    public NamedEntityDto toDto(BaseNamedEntity namedEntity) {
+        return new NamedEntityDto.Builder()
+                .setId(namedEntity)
+                .setName(namedEntity.getName())
+                .build();
+    }
+
+
+    public PlaceDto toDto(Place place) {
+        Collection<ContainerDto> containers = place.getContainers().stream().map(this::toDto).collect(Collectors.toList());;
         Collection<PlaceUserDto> placeUsers = placeUsersToDto(place.getAccounts(), place.getUnaactiveAccounts());
 
         return new PlaceDto.Builder()
@@ -28,35 +31,36 @@ public class EntityToDtoMapper {
                 .build();
     }
 
-    public NamedEntityDto namedEntityToDto(BaseNamedEntity namedEntity) {
-        return new NamedEntityDto.Builder()
-                .setId(namedEntity)
-                .setName(namedEntity.getName())
-                .build();
-    }
-
     public Collection<PlaceUserDto> placeUsersToDto(Collection<Account> active, Collection<Account> unactive) {
         Collection<PlaceUserDto> activeDto = active.stream()
-                .map(this::accountToActivePlaceUserDto)
+                .map(this::activeAccountToDto)
                 .collect(Collectors.toList());;
         Collection<PlaceUserDto> unactiveDto = unactive.stream()
-                .map(this::accountToUnactivePlaceUserDto)
+                .map(this::unactiveAcountToDto)
                 .collect(Collectors.toList());
 
         return Stream.concat(activeDto.stream(), unactiveDto.stream()).collect(Collectors.toList());
     }
 
-
-    public PlaceUserDto accountToActivePlaceUserDto(Account account) {
-        return toPlaceUser(account, true);
+    public ContainerDto toDto(Container container) {
+        return new ContainerDto.Builder()
+                .setId(container)
+                .setName(container.getName())
+                .setPlaceId(container.getPlace())
+                .build();
     }
 
-    public PlaceUserDto accountToUnactivePlaceUserDto(Account account) {
-        return toPlaceUser(account, false);
+
+
+    public PlaceUserDto activeAccountToDto(Account account) {
+        return placeUserToDto(account, true);
     }
 
+    public PlaceUserDto unactiveAcountToDto(Account account) {
+        return placeUserToDto(account, false);
+    }
 
-    private PlaceUserDto toPlaceUser(Account account, boolean status) {
+    private PlaceUserDto placeUserToDto(Account account, boolean status) {
         return new PlaceUserDto.Builder()
                 .setId(account)
                 .setName(account)
@@ -65,32 +69,7 @@ public class EntityToDtoMapper {
     }
 
 
-    public ItemDto itemToDto(Item item) {
-        return new ItemDto.Builder()
-                .setId(item)
-                .setName(item)
-                .setBarcode(item)
-                .setPlaceId(item.getPlace())
-                .setCategoryId(item.getCategory())
-                .setProducer(producerToDto(item.getProducent()))
-                .setDescription(item)
-                .setStorage(item)
-                .setAllergens(item.getAllergens().stream().map(this::allergenToDto).collect(Collectors.toList()))
-                .setIngredients(item.getIngredients().stream().map(this::ingredientToDto).collect(Collectors.toList()))
-                .setNutrition(nutritionToDto(item.getNutrition()))
-                .build();
-    }
-
-
-    public ProducerDto producerToDto(Producent producent) {
-        return new ProducerDto.Builder()
-                .setId(producent)
-                .setName(producent.getName())
-                .build();
-    }
-
-
-    public ItemInstanceDto itemInstanceToDto(ItemInstance itemInstance) {
+    public ItemInstanceDto toDto(ItemInstance itemInstance) {
         return new ItemInstanceDto.Builder()
                 .setId(itemInstance)
                 .setComment(itemInstance.getComment())
@@ -108,20 +87,10 @@ public class EntityToDtoMapper {
                 .build();
     }
 
-
-    public ContainerDto containerToDto(Container container) {
-        return new ContainerDto.Builder()
-                .setId(container)
-                .setName(container.getName())
-                .setPlaceId(container.getPlace())
-                .build();
-    }
-
-
-    public InstanceChangeDto instanceChangeToDto(InstanceChange instanceChange) {
+    public InstanceChangeDto toDto(InstanceChange instanceChange) {
         return new InstanceChangeDto.Builder()
                 .setId(instanceChange)
-                .setItemInstanceDto(this.itemInstanceToDto(instanceChange.getInstance()))
+                .setItemInstanceDto(this.toDto(instanceChange.getInstance()))
                 .setAccountId(instanceChange.getAccount())
                 .setChangeDate(instanceChange.getChangeDate())
                 .setChangeOnInstance(instanceChange.getChangeType())
@@ -129,7 +98,18 @@ public class EntityToDtoMapper {
     }
 
 
-    public WishListItemDto wishListItemToDto(WishListItem WLI) {
+    public WishListDto toDto(WishList WL) {
+        return new WishListDto.Builder()
+                .setId(WL.getId())
+                .setName(WL.getName())
+                .setDescription(WL.getDescription())
+                .setPlaceId(WL.getPlace())
+                .setItems(WL.getItems().stream().map(this::toDto).collect(Collectors.toList()))
+                .setStatus(WL.getStatus())
+                .build();
+    }
+
+    public WishListItemDto toDto(WishListItem WLI) {
         return new WishListItemDto.Builder()
                 .setId(WLI)
                 .setWishListId(WLI.getWishList())
@@ -145,19 +125,31 @@ public class EntityToDtoMapper {
     }
 
 
-    public WishListDto wishListToDto(WishList WL) {
-        return new WishListDto.Builder()
-                .setId(WL.getId())
-                .setName(WL.getName())
-                .setDescription(WL.getDescription())
-                .setPlaceId(WL.getPlace())
-                .setItems(WL.getItems().stream().map(this::wishListItemToDto).collect(Collectors.toList()))
-                .setStatus(WL.getStatus())
+
+    public ItemDto toDto(Item item) {
+        return new ItemDto.Builder()
+                .setId(item)
+                .setName(item)
+                .setBarcode(item)
+                .setPlaceId(item.getPlace())
+                .setCategoryId(item.getCategory())
+                .setProducer(toDto(item.getProducent()))
+                .setDescription(item)
+                .setStorage(item)
+                .setAllergens(item.getAllergens().stream().map(this::toDto).collect(Collectors.toList()))
+                .setIngredients(item.getIngredients().stream().map(this::toDto).collect(Collectors.toList()))
+                .setNutrition(toDto(item.getNutrition()))
                 .build();
     }
 
+    public ProducerDto toDto(Producent producent) {
+        return new ProducerDto.Builder()
+                .setId(producent)
+                .setName(producent.getName())
+                .build();
+    }
 
-    public NutritionDto nutritionToDto(Nutrition nutrition) {
+    public NutritionDto toDto(Nutrition nutrition) {
         return new NutritionDto.Builder()
                 .setId(nutrition)
                 .setEnergy(nutrition.getEnergy())
@@ -170,8 +162,7 @@ public class EntityToDtoMapper {
                 .build();
     }
 
-
-    public AllergenDto allergenToDto(Allergen allergen) {
+    public AllergenDto toDto(Allergen allergen) {
         return new AllergenDto.Builder()
                 .setId(allergen)
                 .setName(allergen.getName())
@@ -179,8 +170,7 @@ public class EntityToDtoMapper {
                 .build();
     }
 
-
-    public IngredientDto ingredientToDto(Ingredient ingredient) {
+    public IngredientDto toDto(Ingredient ingredient) {
         return new IngredientDto.Builder()
                 .setId(ingredient)
                 .setName(ingredient.getName())
