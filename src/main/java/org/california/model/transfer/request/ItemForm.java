24e9386b8059698221d256323152b24607d584e6
Kamil <kamil.gawel.aplikacje@gmail.com>
2019-06-22 14:30:05 +0200

    Updated fields. Fields changed to be publlic. Getter and setters removed.

--- src/main/java/org/california/model/transfer/request/ItemForm.java
+++ src/main/java/org/california/model/transfer/request/ItemForm.java
@@ -2,112 +2,24 @@ package org.california.model.transfer.request;
 
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.Map;
 
 
 public class ItemForm implements Serializable {
 
+    public String name;
+    public Long categoryId;
+    public Long placeId;
 
-    private String name;
-    private Long categoryId;
-    private Long placeId;
+    public long barcode;
+    public String description;
+    public String storage;
 
-    private long barcode;
-    private String description;
-    private String storage;
+    public Map<String, Boolean> allergens;
+    public Collection<String> ingredients;
+    public NutritionForm nutrition;
 
-    private Collection<AllergenForm> allergens;
-    private Collection<IngredientForm> ingredients;
-    private NutritionForm nutrition;
+    public String producer;
+    public String capacity;
 
-    private ProducerForm producer;
-    private Double netWeight;
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public Long getPlaceId() {
-        return placeId;
-    }
-
-    public void setPlaceId(Long placeId) {
-        this.placeId = placeId;
-    }
-
-    public long getBarcode() {
-        return barcode;
-    }
-
-    public void setBarcode(long barcode) {
-        this.barcode = barcode;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getStorage() {
-        return storage;
-    }
-
-    public void setStorage(String storage) {
-        this.storage = storage;
-    }
-
-    public Collection<AllergenForm> getAllergens() {
-        return allergens;
-    }
-
-    public void setAllergens(Collection<AllergenForm> allergens) {
-        this.allergens = allergens;
-    }
-
-    public Collection<IngredientForm> getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(Collection<IngredientForm> ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public NutritionForm getNutrition() {
-        return nutrition;
-    }
-
-    public void setNutrition(NutritionForm nutrition) {
-        this.nutrition = nutrition;
-    }
-
-    public ProducerForm getProducer() {
-        return producer;
-    }
-
-    public void setProducer(ProducerForm producer) {
-        this.producer = producer;
-    }
-
-    public Double getNetWeight() {
-        return netWeight;
-    }
-
-    public void setNetWeight(Double netWeight) {
-        this.netWeight = netWeight;
-    }
 }
