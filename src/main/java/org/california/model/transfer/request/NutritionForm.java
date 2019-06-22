d44589da17a66859b35c1fb1a40acc956d178f5e
Kamil <kamil.gawel.aplikacje@gmail.com>
2019-06-22 14:30:59 +0200

    Fields updated. Fields changed to be public. Removed getters and setters.

--- src/main/java/org/california/model/transfer/request/NutritionForm.java
+++ src/main/java/org/california/model/transfer/request/NutritionForm.java
@@ -4,67 +4,13 @@ import java.io.Serializable;
 
 public class NutritionForm implements Serializable {
 
-    private Integer energy;
-    private Double fat;
-    private Double saturatedFat;
-    private Double carbohydrate;
-    private Double sugar;
-    private Double protein;
-    private Double salt;
+    public Double energyKj;
+    public Double energyKcal;
+    public Double fat;
+    public Double saturatedFat;
+    public Double carbohydrate;
+    public Double sugar;
+    public Double protein;
+    public Double salt;
 
-    public Integer getEnergy() {
-        return energy;
-    }
-
-    public void setEnergy(Integer energy) {
-        this.energy = energy;
-    }
-
-    public Double getFat() {
-        return fat;
-    }
-
-    public void setFat(Double fat) {
-        this.fat = fat;
-    }
-
-    public Double getSaturatedFat() {
-        return saturatedFat;
-    }
-
-    public void setSaturatedFat(Double saturatedFat) {
-        this.saturatedFat = saturatedFat;
-    }
-
-    public Double getCarbohydrate() {
-        return carbohydrate;
-    }
-
-    public void setCarbohydrate(Double carbohydrate) {
-        this.carbohydrate = carbohydrate;
-    }
-
-    public Double getSugar() {
-        return sugar;
-    }
-
-    public void setSugar(Double sugar) {
-        this.sugar = sugar;
-    }
-
-    public Double getProtein() {
-        return protein;
-    }
-
-    public void setProtein(Double protein) {
-        this.protein = protein;
-    }
-
-    public Double getSalt() {
-        return salt;
-    }
-
-    public void setSalt(Double salt) {
-        this.salt = salt;
-    }
 }
