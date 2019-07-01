e3a4ce0e28feeee16c9f1b32772f6d09e40b9ba2
Kamil <kamil.gawel.aplikacje@gmail.com>
2019-07-01 16:27:30 +0200

    Admin controller for creating new items from JSON.

--- src/main/java/org/california/controller/AdminController.java
+++ src/main/java/org/california/controller/AdminController.java
@@ -1,151 +1,71 @@
 package org.california.controller;
 
-import org.california.service.model.CategoryService;
+import org.apache.commons.io.IOUtils;
+import org.california.service.ItemBuilderService;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin
-public class AdminController {
+public class
+AdminController {
+
+
+    private final ItemBuilderService itemBuilderService;
 
     @Autowired
-    CategoryService categoryService;
-
-    @PostMapping("admin/mainProductsList")
-    public boolean addMainProducts() {
-
-//        String list = "Owoce, warzywa\n" +
-//                "        Owoce\n" +
-//                "        Warzywa\n" +
-//                "        Sałatki\n" +
-//                "        Zioła\n" +
-//                "        Grzyby\n" +
-//                "        Orzechy i ziarniste\n" +
-//                "        Owoce suszone i bakalie na wagę\n" +
-//                "Nabiał i jaja\n" +
-//                "        Mleko\n" +
-//                "        Jogurty\n" +
-//                "        Śmietana\n" +
-//                "        Desery mleczne\n" +
-//                "        Ser\n" +
-//                "        Masło i margaryna\n" +
-//                "        Jaja i drożdże\n" +
-//                "        Zdrowa żywność\n" +
-//                "Pieczywo, cukiernia\n" +
-//                "        Chleb\n" +
-//                "        Chleb pakowany\n" +
-//                "        Bułki\n" +
-//                "        Słone przekąski\n" +
-//                "        Słodkie wypieki\n" +
-//                "        Ciasta i desery\n" +
-//                "        Tosty i pieczywo do odpieku\n" +
-//                "        Pieczywo bezglutenowe    \n" +
-//                "Mięso, ryby, garmaż\n" +
-//                "        Mięso i drób na wagę\n" +
-//                "        Mięso i drób paczkowane\n" +
-//                "        Wędliny\n" +
-//                "        Ryby\n" +
-//                "        Garmaż\n" +
-//                "        Dania gotowe\n" +
-//                "Art. spożywcze\n" +
-//                "        Sól i cukier\n" +
-//                "        Mąka\n" +
-//                "        Makaron, ryż i kasza\n" +
-//                "        Płatki śniadaniowe i musli\n" +
-//                "        Olej, oliwa i ocet\n" +
-//                "        Przyprawy\n" +
-//                "        Sosy\n" +
-//                "        Konserwy\n" +
-//                "        Dania gotowe i zupy\n" +
-//                "        Przetwory owocowe, miód\n" +
-//                "        Produkty do pieczenia \n" +
-//                "        Słodycze\n" +
-//                "        Słone przekąski\n" +
-//                "        Kuchnie świata\n" +
-//                "        Zdrowa żywność\n" +
-//                "Mrożonki\n" +
-//                "        Mrożone warzywa\n" +
-//                "        Mrożone owoce\n" +
-//                "        Mrożone mięso i drób\n" +
-//                "        Ryby i owoce morza\n" +
-//                "        Mrożone pizza i frytki\n" +
-//                "        Dania mrożone\n" +
-//                "        Lody\n" +
-//                "Napoje\n" +
-//                "        Herbata\n" +
-//                "        Kawa\n" +
-//                "        Kakao, gorąca czekolada i cappucino\n" +
-//                "        Woda\n" +
-//                "        Soki świeże\n" +
-//                "        Soki, nektary, napoje owocowe\n" +
-//                "        Napoje gazowane\n" +
-//                "        Ice Tea\n" +
-//                "        Syropy\n" +
-//                "        Napoje energetyczne i izotoniczne\n" +
-//                "        Napoje bezalkoholowe\n" +
-//                "Chemia\n" +
-//                "        Proszki do prania, żele, kapsułki\n" +
-//                "        Do płukania tkanin\n" +
-//                "        Zmywanie\n" +
-//                "        Sprzątanie\n" +
-//                "        Akcesoria do sprzątania\n" +
-//                "        Papiery toaletowe \n" +
-//                "        Ręczniki papierowe\n" +
-//                "        Folie\n" +
-//                "        Worki na śmieci\n" +
-//                "        Odświeżacze powietrza\n" +
-//                "        Środki owadobójcze\n" +
-//                "        Pielęgnacja obuwia\n" +
-//                "Kosmetyki\n" +
-//                "        Do twarzy\n" +
-//                "        Do ciała\n" +
-//                "        Pielęgnacja dłoni i stóp\n" +
-//                "        Mydło, do kąpieli\n" +
-//                "        Chusteczki, wata, akcesoria\n" +
-//                "        Do włosów\n" +
-//                "        Do zębów\n" +
-//                "        Golenie\n" +
-//                "        Higiena intymna\n" +
-//                "        Dezodoranty\n" +
-//                "        Wody\n" +
-//                "        Półka zdrowia\n" +
-//                "Dla dzieci\n" +
-//                "        Żywność dla dzieci\n" +
-//                "        Pieluchy i chusteczki\n" +
-//                "        Pielęgnacja\n" +
-//                "        Akcesoria dla dzieci\n" +
-//                "        Akcesoria dla mam\n" +
-//                "Dla zwierząt\n" +
-//                "        Dla kota\n" +
-//                "        Dla psa\n" +
-//                "        Dla gryzoni\n" +
-//                "        Dla rybek\n" +
-//                "        Dla ptaków";
-//
-//        String[] listArray = list.split("\n");
-//
-//        Category parent = new Category();
-//        Category child;
-//
-//        for (String row: listArray) {
-//            System.out.println("Przerabiany row to: " + row);
-//            if(row.startsWith("        ")) {
-//                row = row.replace("        ", "");
-//                child = new Category();
-//                child.setName(row);
-//                child.setParent(parent);
-//                categoryService.(child);
-//            } else {
-//                parent = new Category();
-//                parent.setName(row);
-//                categoryService.create(parent);
-//            }
-//        }
-//
-//    return true;
-        return true;
-
+    public AdminController(ItemBuilderService itemBuilderService) {
+        this.itemBuilderService = itemBuilderService;
     }
+
+    @GetMapping("admin")
+    public ModelAndView samplePage() {
+        return new ModelAndView("index");
+    }
+
+    @GetMapping("admin/uploadProductsForm")
+    public ModelAndView productsForm() {
+        return new ModelAndView("upload");
+    }
+
+    @PostMapping("admin/uploadProducts")
+    public ModelAndView submitProductsFile(@RequestParam("file") MultipartFile file) throws IOException {
+        String content =  IOUtils.toString(file.getInputStream(), StandardCharsets.UTF_8);
+
+        System.out.println(content);
+
+        JSONArray jsonArray = new JSONArray(content);
+
+        Map<JSONObject, Boolean> deserializeResult = itemBuilderService.deserialize(jsonArray);
+
+        Collection<String> successful = deserializeResult.entrySet().stream()
+                .filter(Map.Entry::getValue)
+                .map(Map.Entry::getKey)
+                .map(j -> j.getString("name"))
+                .collect(Collectors.toSet());
+
+        Collection<JSONObject> failed = deserializeResult.entrySet().stream()
+                .filter(e -> !e.getValue())
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+        ModelAndView result = new ModelAndView("upload_status");
+        result.addObject("content", content);
+        result.addObject("succesful", successful);
+        result.addObject("failed", failed);
+        return result;
+    }
+
+
+
 }
