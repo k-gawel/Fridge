df16794cbf961d31ca5bef90b4885c8aa31b3365
Kamil <kamil.gawel.aplikacje@gmail.com>
2019-10-03 22:04:47 +0200

    Fixed little bugs.

--- src/main/java/org/california/controller/ItemInstanceController.java
+++ src/main/java/org/california/controller/ItemInstanceController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 
-@RestController("/item_instances")
-@RequestMapping("/item_instances")
+@RestController("/itemInstances")
+@RequestMapping("/itemInstances")
 @CrossOrigin
 public class ItemInstanceController extends BaseController {
 
